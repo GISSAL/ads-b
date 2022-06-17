ae909e1fd7244294f019b0ca36845214ff347581
Shawn Hutchinson <shutch@ksu.edu>
2022-06-17 11:11:11 -0500

    Update tool_1.py
    Code revision now appends a date string in YYYYMMDD format to the end of the "flight_id" variable.  This will facilitate proper selection of flights later that meet various exclusion criteria.

--- tool_1.py
+++ tool_1.py
@@ -8,7 +8,7 @@
     Description:  ArcGIS script tool code that reads raw ADS-B data from the logger, creates unique flights, and generates output CSV for later GIS operations
     Status:  Development
     Date created: 10/6/2021
-    Date last modified: 6/2/2022
+    Date last modified: 6/17/2022
     Python Version: 3.7
 """
 
@@ -20,12 +20,6 @@ park_name = arcpy.GetParameterAsText(0)
 input_file = arcpy.GetParameterAsText(1)
 dur_threshold = arcpy.GetParameterAsText(2)
 output_workspace = arcpy.GetParameterAsText(3)
-
-# User-specified local variable(s) for stand-alone Python script
-#park_name = "GRSM"
-#input_file = "D:/GIS_Research/ResearchProjects/NPS_ADS_B/SampleData/GRSM/COVEMTN/20190926.tsv"
-#dur_threshold = 900
-#output_workspace = "D:/GIS_Research/ResearchProjects/NPS_ADS_B/Outputs/"
 
 # Start timer
 start = time.time()
@@ -92,8 +86,9 @@ data.drop(data[data["valid_ALTITUDE"] == "False"].index, inplace = True)
 print("Data screened for valid TSLC, Altidude, and Lat/Lon Coordinates...")
 arcpy.AddMessage("Data screened for valid TSLC, Altitudes, and Lat/Lon Coordinates...")
 
-# Convert Unix timestamp to datetime objects in UTC and re-scale select int values 
+# Convert Unix timestamp to datetime objects in UTC and re-scale selected int values 
 data["TIME"] = pd.to_datetime(data["TIME"], unit = "s")
+data["DATE"] = data["TIME"].dt.strftime("%Y%m%d")
 data["lat"] = data["lat"] / 1e7
 data["lon"] = data["lon"] / 1e7
 data["altitude"] = data["altitude"] / 1e3
@@ -122,7 +117,7 @@ arcpy.AddMessage("Identical waypoints removed...")
 # Use threshold waypoint duration value to identify separate flights by aircraft, then sum the number of "true" conditions to assign unique ID
 data['diff_flight'] = data['dur_secs'] >= 900
 data['cumsum'] = data.groupby('ICAO_address')['diff_flight'].cumsum()
-data['flight_id'] = data['ICAO_address'] + "_" + data['cumsum'].astype(str)    
+data['flight_id'] = data['ICAO_address'] + "_" + data['cumsum'].astype(str) + "_" + data['DATE']       
 
 # Remove records where there is only one recorded waypoint
 data = data[data.groupby("flight_id").flight_id.transform(len) > 1]
@@ -130,7 +125,7 @@ print("Separate flights by same aircraft identified...")
 arcpy.AddMessage("Separate flights by same aircraft identified...")
 
 # Remove fields that are no longer needed
-data = data.drop(columns = ['tslc', 'dur_secs', 'diff_flight', 'cumsum', 'valid_BARO', 'valid_VERTICAL_VELOCITY', 'SIMULATED_REPORT', 'valid_IDENT', 'valid_CALLSIGN', 'valid_VELOCITY', 'valid_HEADING', 'valid_ALTITUDE', 'valid_LATLON'])
+data = data.drop(columns = ['tslc', 'dur_secs', 'diff_flight', 'cumsum', 'valid_BARO', 'valid_VERTICAL_VELOCITY', 'SIMULATED_REPORT', 'valid_IDENT', 'valid_CALLSIGN', 'valid_VELOCITY', 'valid_HEADING', 'valid_ALTITUDE', 'valid_LATLON', 'DATE'])
 print("Unused attribute fields removed...")
 arcpy.AddMessage("Unneeded attribute fields and flags removed...")
 
