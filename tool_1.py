b2aa254939d22e3345395e9be78e7787aa1a3044
Shawn Hutchinson <shutch@ksu.edu>
2022-07-26 08:57:38 -0500

    Updates to Tool #1
    A series of revisions to make the script tool more robust in reading in logger files that may have formatting discrepancies, including mixed data types, different header fields, duplicate header lines, or lack of a header.

--- tool_1.py
+++ tool_1.py
@@ -8,12 +8,12 @@
     Description:  ArcGIS script tool code that reads raw ADS-B data from the logger, creates unique flights, and generates output CSV for later GIS operations
     Status:  Development
     Date created: 10/6/2021
-    Date last modified: 6/17/2022
+    Date last modified: 7/26/2022
     Python Version: 3.7
 """
 
 # Import libraries
-import arcpy, os, pandas as pd, time
+import arcpy, os, pandas as pd, time, numpy as np
 
 ## User-specified local variable(s) for ArcGIS script tool
 park_name = arcpy.GetParameterAsText(0)
@@ -24,69 +24,76 @@ output_workspace = arcpy.GetParameterAsText(3)
 # Start timer
 start = time.time()
 
-# Read in ADS-B text file
+# Read in ADS-B text file and check for presence of text header
 data = pd.read_csv(input_file, sep="\t")
+header_list = ["TIME", "timestamp"]
+import_header = data.axes[1]
+result = any(elem in import_header for elem in header_list)
+if result:
+    print("Input file has the required text header...processing will continue!")
+    arcpy.AddMessage("Input file has the required text header...processing will continue!")
+    pass    
+else :
+    print("Input file does not have the required text header...processing cannot proceed!")
+    arcpy.AddMessage("Input file does not have the required text header...processing cannot proceed!")
+    exit()
 
-# Keep only records with valid values
-data = data.dropna(how = 'all')
+# Standardize key field names and remove extra columns collected by the ADS-B data logger
+if "timestamp" in data.columns:
+    data = data.rename(columns={"timestamp":"TIME"})
+if "valid_flags" in data.columns:
+    data = data.rename(columns={"valid_flags":"validFlags"})
+data.drop(["squawk", "altitude_type", "alt_type", "altType", "callsign", "emitter_type", "emitterType"], axis=1, inplace=True, errors="ignore")
+print("Key field names standardized and unused fields removed...")
+arcpy.AddMessage("Key field names standardized and unused fields removed...")
+    
+# Delete duplicate and NA records, including potential duplicate headers
+data = data.drop_duplicates()
+data.dropna(how="any", axis=0, inplace=True)
+print("Duplicate rows and rows having NA values removed...")
+arcpy.AddMessage("Duplicate rows and rows having NA values removed...")
 
-# Ensure remaining field values are in numeric format
-if data["lat"].dtypes != "int64":
-    data["lat"].astype("int")
-else:
-    pass
-
-if data["lon"].dtypes != "int64":
-    data["lon"].astype("int")
-else:
-    pass
-
-if data["altitude"].dtypes != "int64":
-    data["altitude"].astype("int")
-else:
-    pass
-
-if data["heading"].dtypes != "int64":
-    data["heading"].astype("int")
-else:
-    pass
-
-if data["hor_velocity"].dtypes != "int64":
-    data["hor_velocity"].astype("int")
-else:
-    pass
-
-if data["ver_velocity"].dtypes != "int64":
-    data["ver_velocity"].astype("int")
-else:
-    pass
-
-if data["tslc"].dtypes != "int64":
-    data["tslc"].astype("int")
-else:
-    pass
-
-# Unpack TSLC data
+# Unpack validFLags data and convert the 2-byte flag field into a list of T/F values
 flags_names = ["valid_BARO", "valid_VERTICAL_VELOCITY", "SIMULATED_REPORT", "valid_IDENT", "valid_CALLSIGN", "valid_VELOCITY", "valid_HEADING", "valid_ALTITUDE", "valid_LATLON"]
 flags = data["validFlags"].apply(lambda t: list(bin(int(t, 16))[2:].zfill(9)[-9:]))
 flags_df = pd.DataFrame(list(flags), columns=flags_names).replace({'0': False, '1': True})
-data = pd.concat([data.drop('validFlags', axis=1), flags_df], axis=1)
+data = pd.concat([data.drop("validFlags", axis=1), flags_df], axis=1)
+print("Data in validFlags field unpacked and converted to boolean values...")
+arcpy.AddMessage("Data in validFlags field unpacked and converted to boolean values...")
 
+# Keep only those records with valid latlon and altitude values
+data.dropna(how="any", axis=0, inplace=True)
+if data["valid_LATLON"].sum() == len(data.index):
+    invalidLatLon = 0
+else:    
+    invalidLatLon = round(100 - data["valid_LATLON"].sum() / len(data.index) * 100, 2)
+if data["valid_ALTITUDE"].sum() == len(data.index):
+    invalidAltitude = 0
+else:    
+    invalidAltitude = round(100 - data["valid_ALTITUDE"].sum() / len(data.index) * 100, 2)
+data.drop(data[data["valid_LATLON"] == "False"].index, inplace = True)
+data.drop(data[data["valid_ALTITUDE"] == "False"].index, inplace = True)
+print("Rows with invalid Altitudes and Lat/Lon coordinates removed...")
+arcpy.AddMessage("Rows with invalid Altitudes and Lat/Lon coordinates removed...")
+
+# Ensure remaining field values except TIME are in proper numeric format
+data.replace('-', np.NaN, inplace=True)
+data.dropna(how="any", axis=0, inplace=True)
+data["ICAO_address"] = data["ICAO_address"].astype(str)
+data["lat"] = data["lat"].astype(int)
+data["lon"] = data["lon"].astype(int)
+data["altitude"] = data["altitude"].astype(int)
+data["heading"] = data["heading"].astype(int)
+data["hor_velocity"] = data["hor_velocity"].astype(int)
+data["ver_velocity"] = data["ver_velocity"].astype(int)
+data["tslc"] = data["tslc"].astype(int)
 
 # Keep only records with TSLC values of 1 or 2
 invalidTslc = len(data.query("tslc >= 3 or tslc == 0")) / data.shape[0] * 100
 data.drop(data[data["tslc"] >= 3].index, inplace = True)
 data.drop(data[data["tslc"] == 0].index, inplace = True)
 
-# Keep only records with valid latlon and alitude
-invalidAltitude = round(data["valid_ALTITUDE"].value_counts(normalize=True)[0] * 100, 2)
-invalidLatLon = round(data["valid_LATLON"].value_counts(normalize=True)[0] * 100, 2)
-data.drop(data[data["valid_LATLON"] == "False"].index, inplace = True)
-data.drop(data[data["valid_ALTITUDE"] == "False"].index, inplace = True)
-print("Data screened for valid TSLC, Altidude, and Lat/Lon Coordinates...")
-arcpy.AddMessage("Data screened for valid TSLC, Altitudes, and Lat/Lon Coordinates...")
-
-# Convert Unix timestamp to datetime objects in UTC and re-scale selected int values 
+# Convert Unix timestamp to datetime objects in UTC and re-scale selected variable values 
 data["TIME"] = pd.to_datetime(data["TIME"], unit = "s")
 data["DATE"] = data["TIME"].dt.strftime("%Y%m%d")
 data["lat"] = data["lat"] / 1e7
@@ -95,8 +102,15 @@ data["altitude"] = data["altitude"] / 1e3
 data["heading"] = data["heading"] / 1e2
 data["hor_velocity"] = data["hor_velocity"] / 1e2
 data["ver_velocity"] = data["ver_velocity"] / 1e2
-print("ADS-B field data types formatted...")
-arcpy.AddMessage("ADS-B field data types formatted...")
+print("ADS-B field data types formatted and re-scaled...")
+arcpy.AddMessage("ADS-B field data types formatted and re-scaled...")
+
+# Keep only those records with TSLC values of 1 or 2
+invalidTslc = len(data.query("tslc >= 3 or tslc == 0")) / data.shape[0] * 100
+data.drop(data[data["tslc"] >= 3].index, inplace = True)
+data.drop(data[data["tslc"] == 0].index, inplace = True)
+print("Data screened for valid TSLC values...")
+arcpy.AddMessage("Data screened for valid TSLC values...")
 
 # Sort records by ICAO Address and TIME then reset dataframe index
 data.sort_values(["ICAO_address", "TIME"], inplace=True)
