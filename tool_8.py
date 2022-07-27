8e14c058f3d431c2b2580915c6f0f27d701c72e6
Shawn Hutchinson <shutch@ksu.edu>
2022-07-27 12:41:45 -0500

    Tool #8 Update
    Added check out and check in functions for the required Spatial Analyst extension.

--- tool_8.py
+++ tool_8.py
@@ -43,50 +43,61 @@ try:
     
     # Start timer
     start = time.time()
+    
+    if arcpy.CheckExtension("Spatial") == "Available":
+        
+        # Check out Spatial Analyst extension
+        arcpy.CheckOutExtension("Spatial")
        
-    # Calculate overall kernel density (points per square meter)
-    print("Calculating overall kernel density...")
-    arcpy.AddMessage("Calculating overall kernel density...")
-    outKernel = arcpy.sa.KernelDensity(inputFile, "")
-    outKernel.save(os.path.join(outputWorkspace, outputRaster))
-    
-    # Create altitude slices and compute kernel density for each slice
-    print("Calculating kernel density from 0 to {0} MSL...".format(str(altitude1)))
-    arcpy.AddMessage("Calculating kernel density from 0 to {0} MSL...".format(str(altitude1)))
-    selRecs1 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" <= %s """ %altitude1)
-    outKernel1 = arcpy.sa.KernelDensity(selRecs1, "")
-    outKernel1.save(os.path.join(outputWorkspace, outputRaster + "_" + str(altitude1)))
-    
-    print("Calculating kernel density from {0} to {1} MSL...".format(str(altitude1), str(altitude2)))
-    arcpy.AddMessage("Calculating kernel density from {0} to {1} MSL...".format(str(altitude1), str(altitude2)))
-    selRecs2 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" > %s  and "altitude" <= %s """ % (altitude1, altitude2))
-    outKernel2 = arcpy.sa.KernelDensity(selRecs2, "")
-    outKernel2.save(os.path.join(outputWorkspace, outputRaster + "_" + str(altitude2)))
-    
-    print("Calculating kernel density from {0} to {1} MSL...".format(str(altitude2), str(altitude3)))
-    arcpy.AddMessage("Calculating kernel density from {0} to {1} MSL...".format(str(altitude2), str(altitude3)))
-    selRecs3 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" > %s  and "altitude" <= %s """ % (altitude2, altitude3))
-    outKernel3 = arcpy.sa.KernelDensity(selRecs3, "")
-    outKernel3.save(os.path.join(outputWorkspace, outputRaster + "_" + str(altitude3)))
+        # Calculate overall kernel density (points per square meter)
+        print("Calculating overall kernel density...")
+        arcpy.AddMessage("Calculating overall kernel density...")
+        outKernel = arcpy.sa.KernelDensity(inputFile, "")
+        outKernel.save(os.path.join(outputWorkspace, outputRaster))
 
-    print("Calculating kernel density from {0} to {1} MSL...".format(str(altitude3), str(altitude4)))
-    arcpy.AddMessage("Calculating kernel density from {0} to {1} MSL...".format(str(altitude3), str(altitude4)))
-    selRecs4 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" > %s  and "altitude" <= %s """ % (altitude3, altitude4))
-    outKernel4 = arcpy.sa.KernelDensity(selRecs4, "")
-    outKernel4.save(os.path.join(outputWorkspace, outputRaster + "_" + str(altitude4)))
+        # Create altitude slices and compute kernel density for each slice
+        print("Calculating kernel density from 0 to {0} MSL...".format(str(altitude1)))
+        arcpy.AddMessage("Calculating kernel density from 0 to {0} MSL...".format(str(altitude1)))
+        selRecs1 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" <= %s """ %altitude1)
+        outKernel1 = arcpy.sa.KernelDensity(selRecs1, "")
+        outKernel1.save(os.path.join(outputWorkspace, outputRaster + "_" + str(altitude1)))
 
-    print("Calculating kernel density from {0} to {1} MSL...".format(str(altitude4), str(altitude5)))
-    arcpy.AddMessage("Calculating kernel density from {0} to {1} MSL...".format(str(altitude4), str(altitude5)))
-    selRecs5 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" > %s  and "altitude" <= %s """ % (altitude4, altitude5))
-    outKernel5 = arcpy.sa.KernelDensity(selRecs5, "")
-    outKernel5.save(os.path.join(outputWorkspace, outputRaster + "_" + str(altitude5)))
+        print("Calculating kernel density from {0} to {1} MSL...".format(str(altitude1), str(altitude2)))
+        arcpy.AddMessage("Calculating kernel density from {0} to {1} MSL...".format(str(altitude1), str(altitude2)))
+        selRecs2 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" > %s  and "altitude" <= %s """ % (altitude1, altitude2))
+        outKernel2 = arcpy.sa.KernelDensity(selRecs2, "")
+        outKernel2.save(os.path.join(outputWorkspace, outputRaster + "_" + str(altitude2)))
 
-    print("Calculating kernel density above {0} MSL...".format(str(altitude5)))
-    arcpy.AddMessage("Calculating kernel density above {0} MSL...".format(str(altitude5)))
-    selRecs6 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" > %s """ %altitude5)
-    outKernel6 = arcpy.sa.KernelDensity(selRecs6, "")
-    outKernel6.save(os.path.join(outputWorkspace, outputRaster + "_Above_" + str(altitude5)))
-    
+        print("Calculating kernel density from {0} to {1} MSL...".format(str(altitude2), str(altitude3)))
+        arcpy.AddMessage("Calculating kernel density from {0} to {1} MSL...".format(str(altitude2), str(altitude3)))
+        selRecs3 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" > %s  and "altitude" <= %s """ % (altitude2, altitude3))
+        outKernel3 = arcpy.sa.KernelDensity(selRecs3, "")
+        outKernel3.save(os.path.join(outputWorkspace, outputRaster + "_" + str(altitude3)))
+
+        print("Calculating kernel density from {0} to {1} MSL...".format(str(altitude3), str(altitude4)))
+        arcpy.AddMessage("Calculating kernel density from {0} to {1} MSL...".format(str(altitude3), str(altitude4)))
+        selRecs4 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" > %s  and "altitude" <= %s """ % (altitude3, altitude4))
+        outKernel4 = arcpy.sa.KernelDensity(selRecs4, "")
+        outKernel4.save(os.path.join(outputWorkspace, outputRaster + "_" + str(altitude4)))
+
+        print("Calculating kernel density from {0} to {1} MSL...".format(str(altitude4), str(altitude5)))
+        arcpy.AddMessage("Calculating kernel density from {0} to {1} MSL...".format(str(altitude4), str(altitude5)))
+        selRecs5 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" > %s  and "altitude" <= %s """ % (altitude4, altitude5))
+        outKernel5 = arcpy.sa.KernelDensity(selRecs5, "")
+        outKernel5.save(os.path.join(outputWorkspace, outputRaster + "_" + str(altitude5)))
+
+        print("Calculating kernel density above {0} MSL...".format(str(altitude5)))
+        arcpy.AddMessage("Calculating kernel density above {0} MSL...".format(str(altitude5)))
+        selRecs6 = arcpy.management.SelectLayerByAttribute(inputFile, "NEW_SELECTION", """ "altitude" > %s """ %altitude5)
+        outKernel6 = arcpy.sa.KernelDensity(selRecs6, "")
+        outKernel6.save(os.path.join(outputWorkspace, outputRaster + "_Above_" + str(altitude5)))
+        
+        # Check in Spatial Analyst Extension
+        arcpy.CheckInExtension("Spatial")
+        
+    else:
+        arcpy.AddMessage("An ArcGIS Spatial Analyst extension is required!  The current status of this extension is {0}.".format(arcpy.CheckExtension("Spatial")))
+        
 except arcpy.ExecuteError:
     for i in range(0, arcpy.GetMessageCount()):
         arcpy.AddMessage("{0}:  {1}".format(arcpy.GetSeverity(i), arcpy.GetMessage(i)))
