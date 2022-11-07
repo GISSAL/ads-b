67213885d0901521ba6e5a6ef7801981a0ef2e24
Shawn Hutchinson <shutch@ksu.edu>
2022-11-07 07:28:32 -0600

    Update tool_7.py
    Major revision that includes the ability to clip input waypoints by a user-defined buffer distance, changes how output table names are generated, and includes the ArcPy progressor bar with updated messaging.

--- tool_7.py
+++ tool_7.py
@@ -16,19 +16,13 @@
 import arcpy, time
 
 # User-specified local variable(s) for ArcGIS script tool
-inputFile = arcpy.GetParameterAsText(0)
-outputWorkspace = arcpy.GetParameterAsText(1)
-outputTableNameOperator = arcpy.GetParameterAsText(2)
-outputTableNameType = arcpy.GetParameterAsText(3)
-
-# User-specified local variable(s) for stand-alone Python script
-#inputFile = "D:/GIS_Research/ResearchProjects/NPS_ADS_B/NPS_ADS_B_GRSM.gdb/MergedFlightlines"
-#outputWorkspace = "D:/GIS_Research/ResearchProjects/NPS_ADS_B/NPS_ADS_B_GRSM.gdb"
-#outputTableNameOperator = "FlightSummary_Operators"
-#outputTableNameType = "FlightSummary_AircraftType"
+parkName = arcpy.GetParameterAsText(0)
+inputFlightlines = arcpy.GetParameterAsText(1)
+parkBoundaryFile = arcpy.GetParameterAsText(2)
+bufferDistance = arcpy.GetParameterAsText(3)
 
 # Set local environments
-arcpy.env.workspace = outputWorkspace
+arcpy.env.workspace = arcpy.Describe(inputFlightlines).path
 arcpy.env.overwriteOutput = True
 
 # Fixed local variables
@@ -38,59 +32,61 @@ type_reclassTable = [[1, "Glider"], [2, "Balloon"], [3, "Blimp/Dirigible"], [4, 
 
 try:
     
-    # Start timer
+    # Start timer and create progressor
     start = time.time()
+    arcpy.SetProgressor("step", "Creating the buffer and clipping screened flightlines..", 0, 3, 1)
     
-    # Create a temporary copy of the merged flightline file (all flightlines have unique ICAO addresses and flight id's
-    print("Creating a temporary copy of the merged flightline input file...")
-    arcpy.AddMessage("Creating a temporary copy of the merged flightline input file...")    
-    arcpy.management.Copy(inputFile, inputFile + "_Copy")
-    print("Copy of input file created.")
-    arcpy.AddMessage("Copy of input file created.")    
-      
-    print("Calculating flight operator and aircraft type summaries(frequencies and percentages)...") 
-    arcpy.AddMessage("Calculating flight operator and aircraft type summaries(frequencies and percentages)...") 
-    arcpy.analysis.Frequency(inputFile + "_Copy", outputTableNameOperator, "TYPE_REGISTRANT")
-    arcpy.analysis.Frequency(inputFile + "_Copy", outputTableNameType, "TYPE_AIRCRAFT")      
-    with arcpy.da.SearchCursor(outputTableNameOperator, "FREQUENCY") as cursor:
+    # Create buffer polygon around park boundary based on user-defined distance, then clips the inputWaypoits
+    arcpy.analysis.Buffer(parkBoundaryFile, "temp1", bufferDistance)
+    arcpy.analysis.Clip(inputFlightlines,  "temp1", "temp2") 
+    print("Buffer created and flightlines clipped.")    
+    arcpy.AddMessage("Buffer created and flightlines clipped.")    
+    
+    # Calculate frequencies and percentages
+    arcpy.SetProgressorLabel("Calculating flight operator and type frequencies and percentages...")
+    arcpy.SetProgressorPosition()
+    arcpy.analysis.Frequency("temp2", parkName + "_" + "FlightSummary_Operators", "TYPE_REGISTRANT")
+    arcpy.analysis.Frequency("temp2", parkName + "_" + "FlightSummary_Type", "TYPE_AIRCRAFT")      
+    with arcpy.da.SearchCursor(parkName + "_" + "FlightSummary_Operators", "FREQUENCY") as cursor:
         for row in cursor:
             totalFlights += row[0]
-    arcpy.management.AddField(outputTableNameOperator, "PERCENTAGE", "DOUBLE")
-    arcpy.management.AddField(outputTableNameType, "PERCENTAGE", "DOUBLE")    
-    with arcpy.da.UpdateCursor(outputTableNameOperator, ("FREQUENCY", "PERCENTAGE")) as cursor:
+    arcpy.management.AddField(parkName + "_" + "FlightSummary_Operators", "PERCENTAGE", "DOUBLE")
+    arcpy.management.AddField(parkName + "_" + "FlightSummary_Type", "PERCENTAGE", "DOUBLE")    
+    with arcpy.da.UpdateCursor(parkName + "_" + "FlightSummary_Operators", ("FREQUENCY", "PERCENTAGE")) as cursor:
         for frequency, percentage in cursor:            
             cursor.updateRow([frequency, round(frequency / totalFlights * 100, 1)])            
-    with arcpy.da.UpdateCursor(outputTableNameType, ("FREQUENCY", "PERCENTAGE")) as cursor:
+    with arcpy.da.UpdateCursor(parkName + "_" + "FlightSummary_Type", ("FREQUENCY", "PERCENTAGE")) as cursor:
         for frequency, percentage in cursor:            
             cursor.updateRow([frequency, round(frequency / totalFlights * 100, 1)])            
-    print("Flight summaries by operator and aircraft type.") 
-    arcpy.AddMessage("Flight summaries by operator and aircraft type.")
-    
+    print("Aircraft operator and type summaries calculated.") 
+    arcpy.AddMessage("Aircraft operator and type summaries calculated.")
     
     # Reclassify operator and aircraft type numeric codes into descriptive text fields
-    print("Reclassifying operator and aircraft type codes...")
-    arcpy.AddMessage("Reclassifying operator and aircraft type codes...")
-    arcpy.management.ReclassifyField(outputTableNameOperator, "TYPE_REGISTRANT", "MANUAL", "", "", "", operator_reclassTable, "", "Aircraft_Operator")
-    arcpy.management.ReclassifyField(outputTableNameType, "TYPE_AIRCRAFT", "MANUAL", "", "", "", type_reclassTable, "", "Aircraft_Type")
-    arcpy.management.DeleteField(outputTableNameOperator, "Aircraft_Operator_RANGE")
-    #arcpy.management.AlterField(outputTableNameOperator, "Aircraft_Operator_CLASS", "Aircraft_Operator", "Aircraft Operator", "TEXT", 30)
-    arcpy.management.DeleteField(outputTableNameType, "Aircraft_Type_RANGE")
-    #arcpy.management.AlterField(outputTableNameType, "Aircraft_Type_CLASS", "Aircraft_Type", "Aircraft Type", "TEXT", 30)
+    arcpy.SetProgressorLabel("Reclassifying aircraft operators and types...")
+    arcpy.SetProgressorPosition()
+    arcpy.management.ReclassifyField(parkName + "_" + "FlightSummary_Operators", "TYPE_REGISTRANT", "MANUAL", "", "", "", operator_reclassTable, "", "Aircraft_Operator")
+    arcpy.management.ReclassifyField(parkName + "_" + "FlightSummary_Type", "TYPE_AIRCRAFT", "MANUAL", "", "", "", type_reclassTable, "", "Aircraft_Type")
+    arcpy.management.DeleteField(parkName + "_" + "FlightSummary_Operators", "Aircraft_Operator_RANGE")
+    arcpy.management.DeleteField(parkName + "_" + "FlightSummary_Type", "Aircraft_Type_RANGE")
     print("Reclassification complete.")
     arcpy.AddMessage("Reclassification complete.")
-    
-    # Delete copy of temporary merged flightline file
-    print("Deleting copy of merged flightline input file...") 
-    arcpy.AddMessage("Deleting copy of merged flightline input file...")
-    arcpy.management.Delete(inputFile + "_Copy")
-    print("File successfully deleted.") 
-    arcpy.AddMessage("File successfully deleted.")
-  
+
+    # Reset the progressor
+    arcpy.ResetProgressor()
+     
 except arcpy.ExecuteError:
     for i in range(0, arcpy.GetMessageCount()):
-        arcpy.AddMessage("{0}:  {1}".format(arcpy.GetSeverity(i), arcpy.GetMessage(i)))
-
-finally:    
+        arcpy.AddError("{0}:  {1}".format(arcpy.GetSeverity(i), arcpy.GetMessage(i)))
+except:
+    print("An unexpected error occurred processing the input file {0}".format(inputFlightlines))
+    arcpy.AddWarning("An unexpected error occurred processing the input file {0}".format(inputFlightlines))
+    
+finally: 
+    # Delete files no longer needed
+    delList = arcpy.ListFeatureClasses("temp*")
+    for i in delList:
+        arcpy.management.Delete(i)
+    
     # Report execution time
     end = time.time()
     print("Total Execution Time (secs) = {0}".format(str(round(end - start, 3))))    
