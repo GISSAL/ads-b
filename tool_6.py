6114d3901c8a98ab2985bea05ec192c4e568ef55
Shawn Hutchinson <shutch@ksu.edu>
2022-11-07 07:27:08 -0600

    Update tool_6.py
    Major revision that includes the ability to clip input waypoints by a user-defined buffer distance, changes how output table names are generated, and includes the ArcPy progressor bar with updated messaging.

--- tool_6.py
+++ tool_6.py
@@ -16,81 +16,80 @@
 import arcpy, time
 
 # User-specified local variable(s) for ArcGIS script tool
-inputFile = arcpy.GetParameterAsText(0)
-outputWorkspace = arcpy.GetParameterAsText(1)
+parkName = arcpy.GetParameterAsText(0)
+inputFlightlines = arcpy.GetParameterAsText(1)
+parkBoundaryFile = arcpy.GetParameterAsText(2)
+bufferDistance = arcpy.GetParameterAsText(3)
 
 # Set local environments
-arcpy.env.workspace = outputWorkspace
+arcpy.env.workspace = arcpy.Describe(inputFlightlines).path
 arcpy.env.overwriteOutput = True
 
 # Fixed local variables
-totalWaypoints = 0
+totalFlights = 0
+operator_reclassTable = [[1, "Individual"], [2, "Partnership"], [3, "Corporation"], [4, "Co-Owned"], [5, "Government"], [7, "LLC"], [8, "Non-Citizen Corporation"], [9, "Non-Citizen Co-Owned"]]
+type_reclassTable = [[1, "Glider"], [2, "Balloon"], [3, "Blimp/Dirigible"], [4, "Fixed Wing Single Engine"], [5, "Fixed Wing Multi Engine"], [6, "Rotorcraft"], [7, "Weight-Shift-Control"], [8, "Powered Parachute"], [9, "Gyroplane"]]
 
 try:
     
     # Start timer and create progressor
     start = time.time()
-    arcpy.SetProgressor("step", "Creating a temporary copy of the merged waypoint input file and removing identical Flight ID's...", 0, 4, 1)
+    arcpy.SetProgressor("step", "Creating the buffer and clipping screened flightlines..", 0, 3, 1)
     
-    # Create a temporary copy of the merged waypoint file then delete records with identical flight id's
-    # Note:  This keeps only the first waypoint for each flight for summarizing when the flight took place
-    arcpy.management.Copy(inputFile, inputFile + "_Copy")
-    arcpy.management.DeleteIdentical(inputFile + "_Copy", "flight_id")  
-    print("Copy of input file created and identical Flight ID's removed.")    
+    # Create buffer polygon around park boundary based on user-defined distance, then clips the inputWaypoits
+    arcpy.analysis.Buffer(parkBoundaryFile, "temp1", bufferDistance)
+    arcpy.analysis.Clip(inputFlightlines,  "temp1", "temp2") 
+    print("Buffer created and flightlines clipped.")    
+    arcpy.AddMessage("Buffer created and flightlines clipped.")    
     
-    # Convert local DateTime field to text, then parse into HOUR and MONTH fields for later summarization
-    arcpy.SetProgressorLabel("Converting recorded DateTime to local time and creating new hour and month fields...")
+    # Calculate frequencies and percentages
+    arcpy.SetProgressorLabel("Calculating flight operator and type frequencies and percentages...")
     arcpy.SetProgressorPosition()
-    print("Converting recorded DateTime to local time and creating new hour and month fields...")
-    arcpy.management.ConvertTimeField(inputFile + "_Copy", 'TIME', "", "TIME_TEXT", 'TEXT', "yyyy/MM/dd HH:mm:ss")
-    arcpy.management.CalculateField(inputFile + "_Copy", "HOUR", "!TIME_TEXT![-8:-6]", 'PYTHON3')
-    arcpy.management.CalculateField(inputFile + "_Copy", "MONTH", "!TIME_TEXT![-14:-12]", 'PYTHON3')
-    print("Local time, hour, and month fields calculated.")    
-    
-    print("Calculating flight summaries by hour and month (frequencies and percentages)...") 
-    arcpy.SetProgressorLabel("Calculating flight summaries by hour and month (frequencies and percentages)...")
-    arcpy.SetProgressorPosition()    
-    arcpy.analysis.Frequency(inputFile + "_Copy", "WaypointSummary_HR", "HOUR")
-    arcpy.analysis.Frequency(inputFile + "_Copy", "WaypointSummary_MO", "MONTH")      
-    with arcpy.da.SearchCursor("WaypointSummary_HR", "FREQUENCY") as cursor:
+    arcpy.analysis.Frequency("temp2", parkName + "_" + "FlightSummary_Operators", "TYPE_REGISTRANT")
+    arcpy.analysis.Frequency("temp2", parkName + "_" + "FlightSummary_Type", "TYPE_AIRCRAFT")      
+    with arcpy.da.SearchCursor(parkName + "_" + "FlightSummary_Operators", "FREQUENCY") as cursor:
         for row in cursor:
-            totalWaypoints += row[0]
-    arcpy.management.AddField("WaypointSummary_HR", "PERCENTAGE", "DOUBLE")
-    arcpy.management.AddField("WaypointSummary_MO", "PERCENTAGE", "DOUBLE")    
-    with arcpy.da.UpdateCursor("WaypointSummary_HR", ("FREQUENCY", "PERCENTAGE")) as cursor:
+            totalFlights += row[0]
+    arcpy.management.AddField(parkName + "_" + "FlightSummary_Operators", "PERCENTAGE", "DOUBLE")
+    arcpy.management.AddField(parkName + "_" + "FlightSummary_Type", "PERCENTAGE", "DOUBLE")    
+    with arcpy.da.UpdateCursor(parkName + "_" + "FlightSummary_Operators", ("FREQUENCY", "PERCENTAGE")) as cursor:
         for frequency, percentage in cursor:            
-            cursor.updateRow([frequency, round(frequency / totalWaypoints * 100, 1)])            
-    with arcpy.da.UpdateCursor("WaypointSummary_MO", ("FREQUENCY", "PERCENTAGE")) as cursor:
+            cursor.updateRow([frequency, round(frequency / totalFlights * 100, 1)])            
+    with arcpy.da.UpdateCursor(parkName + "_" + "FlightSummary_Type", ("FREQUENCY", "PERCENTAGE")) as cursor:
         for frequency, percentage in cursor:            
-            cursor.updateRow([frequency, round(frequency / totalWaypoints * 100, 1)])            
-    print("Hourly and monthly flight summaries calculated.") 
+            cursor.updateRow([frequency, round(frequency / totalFlights * 100, 1)])            
+    print("Aircraft operator and type summaries calculated.") 
+    arcpy.AddMessage("Aircraft operator and type summaries calculated.")
     
-    # Delete copy of temporary merged waypoint file
-    arcpy.SetProgressorLabel("Deleting copy of merged waypoint input file...")
-    arcpy.SetProgressorPosition()       
-    print("Deleting copy of merged waypoint input file...")     
-    arcpy.management.Delete(inputFile + "_Copy")
-    print("File successfully deleted.") 
-  
-    # Report final aircraft summaries
-    print("Success... Aircraft waypoint hours and months summarized!")
-    arcpy.AddMessage("Success... Aircraft waypoint hours and months summarized!")
-    print("A total of {0} waypoints were used to generate summary data.".format(str(totalWaypoints)))
-    arcpy.AddMessage("A total of {0} waypoints were used to generate summary data.".format(str(totalWaypoints)))
-        
+    # Reclassify operator and aircraft type numeric codes into descriptive text fields
+    arcpy.SetProgressorLabel("Reclassifying aircraft operators and types...")
+    arcpy.SetProgressorPosition()
+    arcpy.management.ReclassifyField(parkName + "_" + "FlightSummary_Operators", "TYPE_REGISTRANT", "MANUAL", "", "", "", operator_reclassTable, "", "Aircraft_Operator")
+    arcpy.management.ReclassifyField(parkName + "_" + "FlightSummary_Type", "TYPE_AIRCRAFT", "MANUAL", "", "", "", type_reclassTable, "", "Aircraft_Type")
+    arcpy.management.DeleteField(parkName + "_" + "FlightSummary_Operators", "Aircraft_Operator_RANGE")
+    arcpy.management.DeleteField(parkName + "_" + "FlightSummary_Type", "Aircraft_Type_RANGE")
+    print("Reclassification complete.")
+    arcpy.AddMessage("Reclassification complete.")
+
     # Reset the progressor
     arcpy.ResetProgressor()
-    
+     
 except arcpy.ExecuteError:
     for i in range(0, arcpy.GetMessageCount()):
-        arcpy.AddMessage("{0}:  {1}".format(arcpy.GetSeverity(i), arcpy.GetMessage(i)))
-
+        arcpy.AddError("{0}:  {1}".format(arcpy.GetSeverity(i), arcpy.GetMessage(i)))
 except:
-    print("An unexpected error occurred processing the input file {0}".format(inputFile))
-    arcpy.AddWarning("An unexpected error occurred processing the input file {0}".format(inputFile))
-
-finally:    
+    print("An unexpected error occurred processing the input file {0}".format(inputFlightlines))
+    arcpy.AddWarning("An unexpected error occurred processing the input file {0}".format(inputFlightlines))
+    
+finally: 
+    # Delete files no longer needed
+    delList = arcpy.ListFeatureClasses("temp*")
+    for i in delList:
+        arcpy.management.Delete(i)
+    
     # Report execution time
     end = time.time()
     print("Total Execution Time (secs) = {0}".format(str(round(end - start, 3))))    
     arcpy.AddMessage("Total Execution Time (secs) = {0}".format(str(round(end - start, 3))))
+    print("There are a total of {0} flights included in the summary.".format(str(totalFlights)))
+    arcpy.AddMessage("There are a total of {0} unique flights included in the summary.".format(str(totalFlights)))
