95abcaa3b199859ecb51fb9893ea24829ddd32c6
Shawn Hutchinson <shutch@ksu.edu>
2024-01-10 11:24:24 -0600

    Update ads_b_tool_1.py
    Added parameter for data logger name and small enhancement to eliminate duplicate data or data outside the area of interest.

--- ads_b_tool_1.py
+++ ads_b_tool_1.py
@@ -8,7 +8,7 @@
     Description:  ArcGIS script tool code that reads raw ADS-B data from the logger, creates unique flights, and generates output CSV for later GIS operations
     Status:  Development
     Date created: 10/6/2021
-    Date last modified: 07/28/2023
+    Date last modified: 01/10/2024
     Python Version: 3.9.16
 """
 
@@ -21,9 +21,10 @@ import arcpy, os, pandas as pd, time, numpy as np
 
 # User-specified local variable(s) for ArcGIS script tool
 park_name = arcpy.GetParameterAsText(0)
-input_file = arcpy.GetParameterAsText(1)
-dur_threshold = arcpy.GetParameterAsText(2)
-output_workspace = arcpy.GetParameterAsText(3)
+logger_name = arcpy.GetParameterAsText(1)
+input_file = arcpy.GetParameterAsText(2)
+dur_threshold = arcpy.GetParameterAsText(3)
+output_workspace = arcpy.GetParameterAsText(4)
 
 try:
     
@@ -36,7 +37,7 @@ try:
     
     # Read in ADS-B text file and check for presence of a single text header row
     # How the pandas read_csv function checks for and warns users of bad lines changed at version 1.3.   
-    # The initial pandas version check ensure users running ArcGIS 2.x and 3.x can run the script.
+    # The initial pandas version check ensures users running ArcGIS 2.x and 3.x can both run the script.
     pd_version = float(pd.__version__[:-2])
     if (pd_version < 1.3):
         data = pd.read_csv(input_file, sep="\t", low_memory=False, error_bad_lines=False, warn_bad_lines=True)
@@ -65,14 +66,6 @@ try:
     print("Key field names standardized and unused fields removed.")
     arcpy.AddMessage("Key field names standardized and unused fields removed.")
     
-    # Delete duplicate and NA records
-    arcpy.SetProgressorLabel("Removing duplicate rows and rows with NA values...")
-    arcpy.SetProgressorPosition()
-    data.drop_duplicates(inplace=True)
-    data.dropna(how="any", axis=0, inplace=True)
-    print("Duplicate rows and rows having NA values removed.")
-    arcpy.AddMessage("Duplicate rows and rows having NA values removed.")
-    
     # Unpack validFLags data and convert the 2-byte flag field into a list of Boolean values
     arcpy.SetProgressorLabel("Unpacking data in validFlags field and converting to boolean values...")
     arcpy.SetProgressorPosition()
@@ -86,7 +79,6 @@ try:
     # Keep only those records with valid latlon and altitude values based on validFlags
     arcpy.SetProgressorLabel("Identifying rows with invalid altitudes and lat/long coordinates...")
     arcpy.SetProgressorPosition()
-    data.dropna(how="any", axis=0, inplace=True)
     if data["valid_LATLON"].sum() == len(data.index):
         invalidLatLon = 0
     else:    
@@ -97,8 +89,7 @@ try:
         invalidAltitude = round(100 - data["valid_ALTITUDE"].sum() / len(data.index) * 100, 2)
     data.drop(data[data["valid_LATLON"] == "False"].index, inplace = True)
     data.drop(data[data["valid_ALTITUDE"] == "False"].index, inplace = True)
-    print("Rows with invalid Altitudes and Lat/Lon coordinates removed.")
-    arcpy.AddMessage("Rows with invalid Altitudes and Lat/Lon coordinates removed.")
+    print("Rows with invalid flags for altitude and lat/lon coordinates removed.")
     
     # Ensure remaining field values (except TIME) use proper data types
     arcpy.SetProgressorLabel("Formatting field values...")
@@ -114,17 +105,19 @@ try:
     data["ver_velocity"] = data["ver_velocity"].astype(int)
     data["tslc"] = data["tslc"].astype(int)
     
-    # Convert Unix-based TIME field to datetime objects in UTC then re-scale selected numeric variable values 
+    # Convert Unix-based TIME field to datetime objects in then re-scale selected numeric variable values 
+    # Note that the TIME field is controlled by the ADS-B logger which is initialized by local users in local time
     arcpy.SetProgressorLabel("Converting TIME field to datetime objects and re-scaling numeric variables...")
     arcpy.SetProgressorPosition()
     data["TIME"] = pd.to_datetime(data["TIME"], unit = "s")
-    data["DATE"] = data["TIME"].dt.strftime("%Y%m%d")
     data["lat"] = data["lat"] / 1e7
     data["lon"] = data["lon"] / 1e7
     data["altitude"] = data["altitude"] / 1e3
     data["heading"] = data["heading"] / 1e2
     data["hor_velocity"] = data["hor_velocity"] / 1e2
     data["ver_velocity"] = data["ver_velocity"] / 1e2
+    data["SITE"] = logger_name
+    data["DATE"] = data["TIME"].dt.strftime("%Y%m%d")
     print("ADS-B field data types properly formatted and re-scaled.")
     arcpy.AddMessage("ADS-B field data types properly formatted and re-scaled.")
     
@@ -136,6 +129,22 @@ try:
     data.drop(data[data["tslc"] == 0].index, inplace = True)
     print("Data screened for valid TSLC values.")
     arcpy.AddMessage("Data screened for valid TSLC values.")
+
+    # Remove lat/lon coordinates that are obviously incorrect
+    invalidLat = len(data.query("lat >= 90 or lat <= -90")) / data.shape[0] * 100
+    invalidLon = len(data.query("lon >= 180 or lon <= -180")) / data.shape[0] * 100
+    data.drop(data[data["lat"] >= 90].index, inplace = True)
+    data.drop(data[data["lat"] <= -90].index, inplace = True)
+    data.drop(data[data["lon"] >= 180].index, inplace = True)
+    data.drop(data[data["lon"] <= -180].index, inplace = True)
+    invalidCoords = invalidLatLon + invalidLat + invalidLon
+    print("Data screened for additional invalid coordinate values.")
+    arcpy.AddMessage("Data screened for additional invalid coordinate values.")
+
+    # Count then delete any duplicate waypoints in a single input file
+    duplicateWaypoints = 100 - (len(data.drop_duplicates(subset=["TIME", "ICAO_address", "lat", "lon", "altitude", "heading"])) / len(data) * 100)
+    data.drop_duplicates(subset=["TIME", "ICAO_address", "lat", "lon", "altitude", "heading"], keep = 'last')
+    print("Duplicate aircraft waypoints removed.")
     
     # Sort records by ICAO_address and TIME then reset dataframe index
     arcpy.SetProgressorLabel("Sorting records by ICAO Address and Time...")
@@ -145,42 +154,44 @@ try:
     print("ADS-B records sorted by ICAO Address and Time.")
     arcpy.AddMessage("ADS-B records sorted by ICAO Address and Time.")
     
+    # Simplify waypoints by removing rows with sequential values
+    preSimplify = len(data)
+    cols = ["ICAO_address", "lat", "lon", "altitude", "heading", "SITE"]
+    data = data.loc[(data[cols].shift() != data[cols]).any(axis=1)]
+    postSimplify = len(data)
+    print("ADS-B records simplified by removing sequential duplicates.")
+    arcpy.AddMessage("ADS-B records simplified by removing sequential duplicates.")
+
+    # Drop fields no longer needed
+    data = data.drop(columns = ['tslc', 'valid_BARO', 'valid_VERTICAL_VELOCITY', 'SIMULATED_REPORT', 'valid_IDENT', 'valid_CALLSIGN', 'valid_VELOCITY', 'valid_HEADING', 'valid_ALTITUDE', 'valid_LATLON'])
+    
     # Calculate time difference between sequential waypoints for each aircraft
     arcpy.SetProgressorLabel("Calculating time difference between waypoints for each aircraft...")
     arcpy.SetProgressorPosition()
     data["dur_secs"] = data.groupby("ICAO_address")["TIME"].diff().dt.total_seconds()
     data["dur_secs"] = data["dur_secs"].fillna(0)
     
-    # Count then delete any identical waypoints in a single input file based on ICAO_address, time, lat, and lon
-    arcpy.SetProgressorLabel("Deleting duplicate aircraft waypoints...")
-    arcpy.SetProgressorPosition()
-    duplicateWaypoints = 100 - (len(data.drop_duplicates(subset=['ICAO_address', 'TIME', 'lat', 'lon'])) / len(data) * 100)
-    data.drop_duplicates(subset=['ICAO_address', 'TIME', 'lat', 'lon'], keep = 'last')
-    print("Identical aircraft waypoints removed.")
-    arcpy.AddMessage("Identical aircraft waypoints removed.")
-    
     # Use threshold waypoint duration value to identify separate flights by an aircraft then sum the number of "true" conditions to assign unique ID's
     arcpy.SetProgressorLabel("Identifying and creating labels for separate flights by the same aircraft...")
     arcpy.SetProgressorPosition()
-    data['diff_flight'] = data['dur_secs'] >= int(dur_threshold)
-    
+    data['diff_flight'] = data['dur_secs'] >= int(dur_threshold)  
     data['cumsum'] = data.groupby('ICAO_address')['diff_flight'].cumsum()
     data['flight_id'] = data['ICAO_address'] + "_" + data['cumsum'].astype(str)  + "_" + data['DATE']   
     print("Separate flights by same aircraft identified.")
-    arcpy.AddMessage("Separate flights by same aircraft identified.")
-   
+    arcpy.AddMessage("Separate flights by same aircraft identified.") 
+
     # Remove records where there is only one recorded waypoint for an aircraft and fields that are no longer needed 
     arcpy.SetProgressorLabel("Removing aircraft records with a single waypoint and unneeded attribute fields...")
     arcpy.SetProgressorPosition()
     data = data[data.groupby("flight_id").flight_id.transform(len) > 1]
-    data = data.drop(columns = ['tslc', 'dur_secs', 'diff_flight', 'cumsum', 'valid_BARO', 'valid_VERTICAL_VELOCITY', 'SIMULATED_REPORT', 'valid_IDENT', 'valid_CALLSIGN', 'valid_VELOCITY', 'valid_HEADING', 'valid_ALTITUDE', 'valid_LATLON', 'DATE'])
-    print("Single waypoint records and unused attribute fields removed.")
-    arcpy.AddMessage("Single waypoint records and unused attribute fields removed.")
+    data = data.drop(columns = ['dur_secs', 'diff_flight', 'cumsum'])
+    print("Single waypoint records and unneeded attribute fields removed.")
+    arcpy.AddMessage("Single waypoint records and unneeded attribute fields removed.") 
     
     # Write output file in CSV format based on the TSV input file name
     arcpy.SetProgressorLabel("Writing the output CSV file...")
     arcpy.SetProgressorPosition()
-    data.to_csv(output_workspace + "\ADSB_" + park_name + "_" + os.path.splitext(base)[0] + ".csv")
+    data.to_csv(output_workspace + "\ADSB_" + park_name + "_" + logger_name + "_" + os.path.splitext(base)[0] + ".csv")
     print("Success... ADS-B data cleaned and formatted output file created!")
     arcpy.AddMessage("Success... ADS-B data cleaned and formatted output file created!")
     
@@ -196,14 +207,16 @@ try:
     arcpy.AddMessage("Percent of original waypoints eliminated by TSLC: {0}".format(str(round(invalidTslc,2))))
     print("Percent of duplicate waypoints: {0}".format(str(round(duplicateWaypoints,2))))
     arcpy.AddMessage("Percent of duplicate waypoints: {0}".format(str(round(duplicateWaypoints,2))))
-    print("Percent of original waypoints with invalid altitudes: {0}".format(str(invalidAltitude)))
-    arcpy.AddMessage("Percent of original waypoints with invalid altitudes: {0}".format(str(invalidAltitude)))
-    print("Percent of waypoints with invalid x,y coordinates: {0}".format(str(invalidLatLon)))
-    arcpy.AddMessage("Percent of waypoints with invalid x,y coordinates: {0}".format(str(invalidLatLon)))
-    print("Total Unique Aircraft in Input File: {0}".format(len(aircraft)))
-    arcpy.AddMessage("Total Unique Aircraft in Input File: {0}".format(len(aircraft)))
+    print("Percent of waypoints with invalid altitudes: {0}".format(str(invalidAltitude)))
+    arcpy.AddMessage("Duplicate Sequential Waypoints Deleted: {0}".format(str(round(preSimplify-postSimplify,2))))
+    print("Percent of waypoints with invalid altitudes: {0}".format(str(round(preSimplify-postSimplify,2))))
+    arcpy.AddMessage("Percent of waypoints with invalid altitudes: {0}".format(str(invalidAltitude)))
+    print("Percent of waypoints with invalid x,y coordinates: {0}".format(str(round(invalidCoords, 2))))
+    arcpy.AddMessage("Percent of waypoints with invalid x,y coordinates: {0}".format(str(round(invalidCoords, 2))))
     print("Total Flights in Input File: {0}".format(len(flights)))
     arcpy.AddMessage("Total Flights in Input File: {0}".format(len(flights)))
+    print("Total Unique Aircraft in Input File: {0}".format(len(aircraft)))
+    arcpy.AddMessage("Total Unique Aircraft in Input File: {0}".format(len(aircraft)))
     print("Total Execution Time (secs) = {0}".format(str(round(end - start, 3))))
     arcpy.AddMessage("Total Execution Time (secs) = {0}".format(str(round(end - start, 3))))
 
